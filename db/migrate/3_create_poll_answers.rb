214fac483ccfa7b0504981847940f6fc8cdcbbb3
compscidude <grahammeuller@hotmail.com>
2017-04-18 04:13:43 +0900

    migration order modified

---
+++ db/migrate/3_create_poll_answers.rb
@@ -0,0 +1,12 @@
+class CreatePollAnswers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :poll_answers do |t|
+      t.string :answer
+      t.references :poll, foreign_key: true
+      t.integer :vote_count
+
+      t.timestamps
+    end
+    
+  end
+end
