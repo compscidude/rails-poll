19388443d8c1ea2452c04f7503cb942a165568c7
compscidude <grahammeuller@hotmail.com>
2017-04-18 04:08:52 +0900

    Added foreign key to migration files

--- db/migrate/20170417074112_create_polls.rb
+++ db/migrate/20170417074112_create_polls.rb
@@ -1,11 +1,14 @@
 class CreatePolls < ActiveRecord::Migration[5.0]
   def change
     create_table :polls do |t|
-      t.references :author, foreign_key: true
+      t.references :author, index: true
       t.string :question
       t.integer :status
 
       t.timestamps
     end
+
+    add_foreign_key :poll, :users, column: :author_id 
+
   end
 end
--- db/migrate/20170417074146_create_poll_answers.rb
+++ db/migrate/20170417074146_create_poll_answers.rb
@@ -7,5 +7,6 @@ class CreatePollAnswers < ActiveRecord::Migration[5.0]
 
       t.timestamps
     end
+    
   end
 end
